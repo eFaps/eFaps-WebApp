d67b890012535c0962ae0e04f7b7d421cf254066
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-04 18:27:20 +0000

    -bugFix: The valupicker must be deactivated for a Datefield
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@2562 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/ui/wicket/components/form/cell/ValueCellPanel.java
+++ src/main/java/org/efaps/ui/wicket/components/form/cell/ValueCellPanel.java
@@ -48,118 +48,110 @@ import org.efaps.ui.wicket.models.objects.UIForm;
 /**
  * Class renders a cell in a Form.
  *
- * @author jmox
+ * @author The eFaps Team
  * @version $Id$
  */
-public class ValueCellPanel extends Panel {
+public class ValueCellPanel extends Panel
+{
+    /**
+     * Needed for serialization.
+     */
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * Needed for serialization.
-   */
-  private static final long serialVersionUID = 1L;
+    /**
+     * Instance field needed in case that a field needs a datefield.
+     */
+    private DateFieldWithPicker dateTextField = null;
 
-  /**
-   * Instance field needed in case that a field needs a datefield.
-   */
-  private DateFieldWithPicker dateTextField = null;
+    /**
+     * Constructor.
+     *
+     * @param _wicketId wicket id of the component
+     * @param _model model of the component
+     * @param _formmodel model of the form
+     * @param _ajaxLink is this panel an ajax link
+     */
+    public ValueCellPanel(final String _wicketId, final IModel<UIFormCell> _model, final UIForm _formmodel,
+                    final boolean _ajaxLink)
+    {
+        super(_wicketId, _model);
+        setOutputMarkupId(true);
+        final UIFormCell uiFormCell = (UIFormCell) super.getDefaultModelObject();
+        // if we don't have a reference or we are inside a modal window
+        if (uiFormCell.getReference() == null || _formmodel.getTarget().equals(Target.MODAL)) {
+            if (uiFormCell.getIcon() == null) {
+                this.add(new WebComponent("icon").setVisible(false));
+            } else {
+                this.add(new StaticImageComponent("icon", uiFormCell.getIcon()));
+            }
+            // in case of create or edit for a Date
+            if ((_formmodel.isCreateMode() || _formmodel.isEditMode()) && "Date".equals(uiFormCell.getTypeName())) {
 
+                final Date date = (uiFormCell.getCompareValue() == null
+                                    || !(uiFormCell.getCompareValue() instanceof DateTime))
+                                  ? new Date()
+                                  : ((DateTime) uiFormCell.getCompareValue()).toDate();
 
-  /**
-   * Constructor.
-   *
-   * @param _wicketId     wicket id of the component
-   * @param _model        model of the component
-   * @param _formmodel    model of the form
-   * @param _ajaxLink     is this panel an ajax link
-   */
-  public ValueCellPanel(final String _wicketId, final IModel<UIFormCell> _model,
-                        final UIForm _formmodel, final boolean _ajaxLink) {
-    super(_wicketId, _model);
-    setOutputMarkupId(true);
-    final UIFormCell uiFormCell = (UIFormCell) super.getDefaultModelObject();
-    // if we don't have a reference or we are inside a modal window
-    if (uiFormCell.getReference() == null
-              || _formmodel.getTarget().equals(Target.MODAL)) {
-      if (uiFormCell.getIcon() == null) {
-        this.add(new WebComponent("icon").setVisible(false));
-      } else {
-        this.add(new StaticImageComponent("icon", uiFormCell.getIcon()));
-      }
-      // in case of create or edit for a Date
-      if ((_formmodel.isCreateMode() || _formmodel.isEditMode())
-          && "Date".equals(uiFormCell.getTypeName())) {
+                this.dateTextField = new DateFieldWithPicker("label", new Model<Date>(date), new StyleDateConverter(
+                                false), uiFormCell.getName());
 
-        final Date date = (uiFormCell.getCompareValue() == null
-                        || !(uiFormCell.getCompareValue() instanceof DateTime))
-                           ? new Date()
-                           : ((DateTime) uiFormCell.getCompareValue()).toDate();
+                this.add(this.dateTextField);
+                this.add(new WebComponent("valuePicker").setVisible(false));
+            } else {
+                if (uiFormCell.isAutoComplete() && (_formmodel.isCreateMode() || _formmodel.isSearchMode())) {
+                    this.add(new AutoCompleteField("label", _model));
+                } else {
+                    if (uiFormCell.isValuePicker() && uiFormCell.render()) {
+                        final Value4Picker value = new Value4Picker("label", _model);
+                        value.setValue(_formmodel.isSearchMode() ? "*" : "0", _formmodel.isSearchMode() ? "*" : "0");
+                        this.add(value);
+                        this.add(new ValuePicker("valuePicker", _model, value));
 
-        this.dateTextField = new DateFieldWithPicker("label",
-                                                  new Model<Date>(date),
-                                                  new StyleDateConverter(false),
-                                                  uiFormCell.getName());
+                    } else {
+                        this.add(new LabelComponent("label", new Model<String>(uiFormCell.getCellValue()))
+                                        .setOutputMarkupId(true));
 
-        this.add(this.dateTextField);
+                        this.add(new WebComponent("valuePicker").setVisible(false));
+                    }
+                }
+            }
+            this.add(new WebMarkupContainer("link").setVisible(false));
 
-      } else {
-        if (uiFormCell.isAutoComplete()
-            && (_formmodel.isCreateMode() || _formmodel.isSearchMode())) {
-          this.add(new AutoCompleteField("label", _model));
         } else {
-          if (uiFormCell.isValuePicker() && uiFormCell.render()) {
-           final Value4Picker value = new Value4Picker("label", _model);
-           value.setValue(_formmodel.isSearchMode() ? "*" : "0",
-                          _formmodel.isSearchMode() ? "*" : "0");
-           this.add(value);
-           this.add(new ValuePicker("valuePicker", _model, value));
-
-          } else {
-            this.add(new LabelComponent("label",
-                new Model<String>(uiFormCell.getCellValue()))
-                                                     .setOutputMarkupId(true));
-
+            this.add(new WebComponent("icon").setVisible(false));
+            this.add(new WebComponent("label").setVisible(false));
             this.add(new WebComponent("valuePicker").setVisible(false));
-          }
+
+            WebMarkupContainer link;
+            if (_ajaxLink && uiFormCell.getTarget() != Target.POPUP) {
+                link = new AjaxLinkContainer("link", _model);
+            } else {
+                link = new ContentContainerLink<UIFormCell>("link", _model);
+                if (uiFormCell.getTarget() == Target.POPUP) {
+                    final PopupSettings popup = new PopupSettings(PageMap.forName("popup"));
+                    ((ContentContainerLink<?>) link).setPopupSettings(popup);
+                }
+            }
+            if (uiFormCell.getIcon() == null) {
+                link.add(new WebComponent("linkIcon").setVisible(false));
+            } else {
+                link.add(new StaticImageComponent("linkIcon", uiFormCell.getIcon()));
+            }
+            link.add(new LabelComponent("linkLabel", new Model<String>(uiFormCell.getCellValue())));
+            this.add(link);
         }
-      }
-      this.add(new WebMarkupContainer("link").setVisible(false));
+    }
 
-    } else {
-      this.add(new WebComponent("icon").setVisible(false));
-      this.add(new WebComponent("label").setVisible(false));
-      this.add(new WebComponent("valuePicker").setVisible(false));
-
-      WebMarkupContainer link;
-      if (_ajaxLink && uiFormCell.getTarget() != Target.POPUP) {
-        link = new AjaxLinkContainer("link", _model);
-      } else {
-        link = new ContentContainerLink<UIFormCell>("link", _model);
-        if (uiFormCell.getTarget() == Target.POPUP) {
-          final PopupSettings popup =
-              new PopupSettings(PageMap.forName("popup"));
-         ((ContentContainerLink<?>) link).setPopupSettings(popup);
+    /**
+     * After rendering the datefields are added to the parent.
+     */
+    @Override
+    protected void onAfterRender()
+    {
+        super.onAfterRender();
+        if (this.dateTextField != null) {
+            final FormPanel formpanel = this.findParent(FormPanel.class);
+            formpanel.addDateComponent(this.dateTextField);
         }
-      }
-      if (uiFormCell.getIcon() == null) {
-        link.add(new WebComponent("linkIcon").setVisible(false));
-      } else {
-        link.add(new StaticImageComponent("linkIcon", uiFormCell.getIcon()));
-      }
-      link.add(new LabelComponent("linkLabel",
-                                 new Model<String>(uiFormCell.getCellValue())));
-      this.add(link);
     }
-  }
-
-  /**
-   * After rendering the datefields are added to the parent.
-   */
-  @Override
-  protected void onAfterRender() {
-    super.onAfterRender();
-    if (this.dateTextField != null) {
-      final FormPanel formpanel = this.findParent(FormPanel.class);
-      formpanel.addDateComponent(this.dateTextField);
-    }
-  }
 }
