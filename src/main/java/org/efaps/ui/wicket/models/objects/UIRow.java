c1cee1932a5922e34a40a81471e0b74f74c8db0d
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-26 19:27:22 +0000

    - changes due to change of interface in the kernel
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@2695 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/ui/wicket/models/objects/UIRow.java
+++ src/main/java/org/efaps/ui/wicket/models/objects/UIRow.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import org.apache.wicket.IClusterable;
 
-import org.efaps.admin.datamodel.AttributeTypeInterface;
 import org.efaps.ui.wicket.models.cell.UITableCell;
 
 /**
