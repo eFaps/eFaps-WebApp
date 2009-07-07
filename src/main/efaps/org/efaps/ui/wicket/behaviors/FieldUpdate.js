b84f172190323ac56f4eb54fe46809b7868bc399
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-07-07 17:43:10 +0000

    - bugFix: missing check for array length led to error
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@2747 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/org/efaps/ui/wicket/behaviors/FieldUpdate.js
+++ src/main/efaps/org/efaps/ui/wicket/behaviors/FieldUpdate.js
@@ -39,41 +39,44 @@ function eFapsSetFieldValue(_referenceId, _fieldName, _fieldValue) {
     }
     // get the field collection 
     var fields = document.getElementsByName(_fieldName);
-    var cp = 0;
-    if (fields.length > 1) {
-        var cp = pos;
-    }
-    if (_fieldValue instanceof Array) {
-        var sel = fields[cp];
-        if (sel.nodeName == 'SELECT') {
-        } else {
-            sel = document.createElement('SELECT');
-            for (i = 0; i< fields[cp].attributes.length;i++) {
-                if (sel.getAttributeNode(fields[cp].attributes[i].name)==null) {
-                    sel.setAttribute(fields[cp].attributes[i].name, fields[cp].attributes[i].value);
-                }
-            }
-            sel.size=1;
-            fields[cp].parentNode.replaceChild(sel, fields[cp]);
+    //only if the field exist go on
+    if (fields.length > 0) { 
+        var cp = 0;
+        if (fields.length > 1) {
+            var cp = pos;
         }
-        while (sel.options.length) {
-            sel.options[0] = null;
-        }
-        for (i = 1; i < _fieldValue.length; i = i + 2) {
-            option = new Option(_fieldValue[i + 1], _fieldValue[i], false,
-                    _fieldValue[i] == _fieldValue[0]);
-            sel.options[sel.length] = option;
-        }
-    } else {
-        // if it is an input, the value can be set directly, else the dom must be used 
-        if (fields[cp].nodeName == 'INPUT') {
-            fields[cp].value = _fieldValue;
-        } else {
-            if (fields[cp].hasChildNodes()) {
-                fields[cp].firstChild.data = _fieldValue;
+        if (_fieldValue instanceof Array) {
+            var sel = fields[cp];
+            if (sel.nodeName == 'SELECT') {
             } else {
-                var n = document.createTextNode(_fieldValue);
-                fields[cp].appendChild(n);
+                sel = document.createElement('SELECT');
+                for (i = 0; i< fields[cp].attributes.length;i++) {
+                    if (sel.getAttributeNode(fields[cp].attributes[i].name)==null) {
+                        sel.setAttribute(fields[cp].attributes[i].name, fields[cp].attributes[i].value);
+                    }
+                }
+                sel.size=1;
+                fields[cp].parentNode.replaceChild(sel, fields[cp]);
+            }
+            while (sel.options.length) {
+                sel.options[0] = null;
+            }
+            for (i = 1; i < _fieldValue.length; i = i + 2) {
+                option = new Option(_fieldValue[i + 1], _fieldValue[i], false,
+                        _fieldValue[i] == _fieldValue[0]);
+                sel.options[sel.length] = option;
+            }
+        } else {
+            // if it is an input, the value can be set directly, else the dom must be used 
+            if (fields[cp].nodeName == 'INPUT') {
+                fields[cp].value = _fieldValue;
+            } else {
+                if (fields[cp].hasChildNodes()) {
+                    fields[cp].firstChild.data = _fieldValue;
+                } else {
+                    var n = document.createTextNode(_fieldValue);
+                    fields[cp].appendChild(n);
+                }
             }
         }
     }
