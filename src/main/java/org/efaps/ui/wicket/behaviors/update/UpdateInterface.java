73658cdf0f93703516a8d35926f72abc975d2dff
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-12-01 03:16:39 +0000

    - JavaDoc only
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@5889 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/ui/wicket/behaviors/update/UpdateInterface.java
+++ src/main/java/org/efaps/ui/wicket/behaviors/update/UpdateInterface.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2009 The eFaps Team
+ * Copyright 2003 - 2010 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,54 +19,53 @@
  */
 
 package org.efaps.ui.wicket.behaviors.update;
-import org.efaps.admin.ui.AbstractUserInterfaceObject.TargetMode;
 
+import org.efaps.admin.ui.AbstractUserInterfaceObject.TargetMode;
 
 /**
  * This Interface is used for Behaviors which are stored in
- * {@link #org.efaps.ui.wicket.EFapsSession()}. For an example of implementing
- * this Interface
+ * {@link #org.efaps.ui.wicket.EFapsSession()}. For an example
+ * of implementing this Interface
  * {@link #org.efaps.ui.wicket.behaviors.update.AbstractAjaxUpdateBehavior()}
  *
- * @see #org.efaps.ui.wicket.behaviors.update.AbstractAjaxUpdateBehavior
- * @author jmox
+ * @author The eFaps TEam
  * @version $Id$
  */
-public interface UpdateInterface {
+public interface UpdateInterface
+{
 
-  /**
-   * This method should return if the class should respond on a Ajaxrequest.
-   *
-   * @return true if ajax
-   */
-  boolean isAjaxCallback();
+    /**
+     * This method should return if the class should respond on a Ajaxrequest.
+     *
+     * @return true if ajax
+     */
+    boolean isAjaxCallback();
 
-  /**
-   * Thi smethod should return the CallBackScript of the Behavior.
-   *
-   * @return ajax callback
-   */
-  String getAjaxCallback();
+    /**
+     * Thi smethod should return the CallBackScript of the Behavior.
+     *
+     * @return ajax callback
+     */
+    String getAjaxCallback();
 
-  /**
-   * Method used to set the Oid.
-   *
-   * @param _instanceKey oid to set
-   */
-  void setInstanceKey(final String _instanceKey);
+    /**
+     * Method used to set the Oid.
+     *
+     * @param _instanceKey oid to set
+     */
+    void setInstanceKey(final String _instanceKey);
 
-  /**
-   * Method used to set the TargetMode.
-   *
-   * @param _mode mode to set
-   */
-  void setMode(final TargetMode _mode);
+    /**
+     * Method used to set the TargetMode.
+     *
+     * @param _mode mode to set
+     */
+    void setMode(final TargetMode _mode);
 
-  /**
-   * returns the Id of this Behavior.
-   *
-   * @return id
-   */
-  String getId();
-
+    /**
+     * returns the Id of this Behavior.
+     *
+     * @return id
+     */
+    String getId();
 }
