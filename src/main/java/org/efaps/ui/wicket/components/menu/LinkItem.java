f9ed59d4b40860577d3dbecec118c0788612fce0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-06-22 17:37:57 +0000

    - correction of html tags in webapp
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@7707 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/ui/wicket/components/menu/LinkItem.java
+++ src/main/java/org/efaps/ui/wicket/components/menu/LinkItem.java
@@ -168,7 +168,7 @@ public class LinkItem
         super.onComponentTagBody(_markupStream, _openTag);
         final StringBuilder html = new StringBuilder();
         if (getModelObject().getImage() == null) {
-            html.append("<span class=\"eFapsMenuImagePlaceHolder\">").append("&nbsp;</span>");
+            html.append("<div class=\"eFapsMenuImagePlaceHolder\">").append("&nbsp;</div>");
         } else {
             html.append("<img src=\"/..").append(getModelObject().getImage()).append("\" class=\"eFapsMenuImage\"/>");
         }
