fc560c1b8aace222f7a21ce6446ebc81e3143ef0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-08-02 22:04:49 +0000

    - webapp: reordering packages
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-WebApp/trunk@9946 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/java/org/efaps/ui/wicket/components/bpm/process/ProcessAdminPanel.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.ui.wicket.components.bpm.process;
+
+import org.apache.wicket.PageReference;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.efaps.util.EFapsException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class ProcessAdminPanel
+    extends Panel
+{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param _id
+     */
+    public ProcessAdminPanel(final String _id,
+                             final PageReference _pageReference)
+        throws EFapsException
+    {
+        super(_id);
+        // TODO Auto-generated constructor stub
+    }
+
+}
