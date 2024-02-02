9cdc8585d443efa7c0e9d6074da8d283f803b441
Jan Moxter <jan@moxter.net>
2024-02-01 21:23:43 -0500

    Set headers

--- src/main/java/org/efaps/ui/wicket/components/menu/ajax/SetCompanyItem.java
+++ src/main/java/org/efaps/ui/wicket/components/menu/ajax/SetCompanyItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2014 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Revision:        $Rev:1510 $
- * Last Changed:    $Date:2007-10-18 09:35:40 -0500 (Thu, 18 Oct 2007) $
- * Last Changed By: $Author:jmox $
  */
-
 package org.efaps.ui.wicket.components.menu.ajax;
 
 import org.apache.wicket.Page;
